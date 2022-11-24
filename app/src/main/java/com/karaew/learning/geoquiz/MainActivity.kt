fa43db435c58f196e127d30a0da0176297867906
Theyoo <andreyka.karaev.1999@mail.ru>
2022-11-25 00:37:16 +0300

    upd add question

--- app/src/main/java/com/karaew/learning/geoquiz/MainActivity.kt
+++ app/src/main/java/com/karaew/learning/geoquiz/MainActivity.kt
@@ -10,6 +10,16 @@ import android.widget.Toast
 private lateinit var buttonTrue: Button
 private lateinit var buttonFalse: Button
 
+private val questionBank = listOf(
+    Question(R.string.question_australia, true),
+    Question(R.string.question_oceans, true),
+    Question(R.string.question_mideast, false),
+    Question(R.string.question_africa, false),
+    Question(R.string.question_americas, true),
+    Question(R.string.question_asia, true)
+)
+private var currentIndex = 0
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
